2a5fd38c404c391b35fee3212383887c7855cf13
Will Dower <wdower@mitre.org>
2020-04-10 16:30:07 -0400

    added transpileDependencies for Edge support
    Signed-off-by: Will Dower <wdower@mitre.org>

--- src/router.js
+++ src/router.js
@@ -2,22 +2,22 @@ import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
 import communityData from "./assets/data/communityData.json";
-import goTo from 'vuetify/es5/services/goto';
+// import goTo from 'vuetify/es5/services/goto';
 
 Vue.use(Router);
 
 const router = new Router({
 //  mode: "history",
 //  base: process.env.BASE_URL,
-  scrollBehavior(to) {
-    let scrollTo = 0
-    if (to.hash) {
-      scrollTo = to.hash
-    } else {
-      scrollTo = 0
-    }
-    return goTo(scrollTo, { 'duration': 0 })
-  },
+  // scrollBehavior(to) {
+  //   let scrollTo = 0
+  //   if (to.hash) {
+  //     scrollTo = to.hash
+  //   } else {
+  //     scrollTo = 0
+  //   }
+  //   return goTo(scrollTo, { 'duration': 0 })
+  // },
   routes: [
     {
       path: "/",
