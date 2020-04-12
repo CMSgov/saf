99278dd53b64aa6aef94c4d969c512016e6eff9d
wdower <wdower@users.noreply.github.com>
2020-04-12 18:39:33 +0000

    deploy: 4650f2d7d66ff176c71a6a4b69916864038ec159

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/saf/precache-manifest.0c7a10e5541d669fa320f6008b84bada.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("/saf/precache-manifest.2c36543b489c0d80c65e545cef711476.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 self.addEventListener("message", e => {
   if (!e.data) {
