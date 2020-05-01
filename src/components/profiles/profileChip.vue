4da26fff9fe47f016fef2c8c05b8fd455eba4fce
Will Dower <wdower@mitre.org>
2020-05-01 11:58:09 -0400

    new components for the profiles
    Signed-off-by: Will Dower <wdower@mitre.org>

---
+++ src/components/profiles/profileChip.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-chip
+    v-if="entry.link"
+    :href="entry.link"
+    target="_blank"
+    class="google-font break-word ma-2 pa-2 text-none"
+    label
+    outlined
+  >
+    <v-img
+      v-show="entry.svg"
+      :src="require('@/assets/img/svg/' + entry.svg + '.svg')"
+      svg-inline
+      max-width="15"
+      max-height="15"
+      class="mr-2"
+    />
+
+    {{ entry.name }}
+  </v-chip>
+</template>
+
+<script>
+export default {
+  props: {
+    entry: Object
+  }
+};
+</script>
