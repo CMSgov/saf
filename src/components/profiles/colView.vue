55a3fd536851b0fefb377c66f3fcd95b46c6fe83
Will Dower <wdower@mitre.org>
2020-05-04 13:47:08 -0400

    added outlines to rows, changed around breakpoints and row/column layout to resize easier, added simplified mobile view (colView), broke code out into a rowView for big screens and a colView for small ones, fixed typos
    Signed-off-by: Will Dower <wdower@mitre.org>

---
+++ src/components/profiles/colView.vue
@@ -0,0 +1,48 @@
+<template>
+
+      <v-container class="pa-0 px-2" fluid>
+        <v-row justify="center">
+            <profileChip v-if="getByFips(this.entry, 'none')" :entry="getByFips(this.entry, 'none')" />
+            <div class="body-2 break-word ma-2" v-else>{{entry.name}}</div>
+          </v-col>
+          <v-col cols="auto" class="pa-0 ma-2" v-if="getByFips(entry, 'low')">
+            <profileChip :entry="getByFips(entry, 'low')" />
+          </v-col>
+          <v-col cols="auto" class="pa-0 ma-2" v-if="getByFips(this.entry, 'mod')">
+            <profileChip :entry="getByFips(entry, 'mod')" />
+          </v-col>
+          <v-col cols="auto" class="pa-0 ma-2" v-if="getByFips(this.entry, 'high')">
+            <profileChip :entry="getByFips(entry, 'high')" />
+          </v-col>
+        </v-row>
+      </v-container>
+
+</template>
+
+<script>
+import profileChip from "@/components/profiles/profileChip.vue";
+export default {
+  components: {
+    profileChip
+  },
+  props: {
+      entry: Object
+  },
+  methods: {
+    getByFips(profile, fips_cat) {
+      var i;
+      var j;
+      //console.log(profile.links.length)
+      for (i = 0; i < profile.links.length; i++) {
+        for (j = 0; j < profile.links[i].fips.length; j++) {
+          //console.log(profile.links[i].fips)
+          if (profile.links[i].fips[j] == fips_cat) {
+            //console.log(profile.links[i]);
+            return profile.links[i];
+          }
+        }
+      }
+    }
+  }
+};
+</script>
---
+++ src/components/profiles/rowView.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-row dense align="center">
+    <v-col :cols="getByFips(entry, 'none') ? '12' : '4'" >
+      <profileChip v-if="getByFips(entry, 'none')" :entry="getByFips(entry, 'none')" />
+      <div class="body-2 break-word" v-else>{{entry.name}}</div>
+    </v-col>
+    <v-col cols="8" class="pa-0">
+      <v-container class="pa-0 px-2" v-if="!getByFips(entry, 'none')" fluid>
+        <v-row align="center">
+          <v-col class="pa-0 ma-2">
+            <profileChip :entry="getByFips(entry, 'low')" />
+          </v-col>
+          <v-col class="pa-0 ma-2">
+            <profileChip :entry="getByFips(entry, 'mod')" />
+          </v-col>
+          <v-col class="pa-0 ma-2">
+            <profileChip :entry="getByFips(entry, 'high')" />
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import profileChip from "@/components/profiles/profileChip.vue";
+export default {
+  components: {
+    profileChip
+  },
+  props: {
+      entry: Object
+  },
+  methods: {
+    getByFips(profile, fips_cat) {
+      var i;
+      var j;
+      //console.log(profile.links.length)
+      for (i = 0; i < profile.links.length; i++) {
+        for (j = 0; j < profile.links[i].fips.length; j++) {
+          //console.log(profile.links[i].fips)
+          if (profile.links[i].fips[j] == fips_cat) {
+            //console.log(profile.links[i]);
+            return profile.links[i];
+          }
+        }
+      }
+    }
+  }
+};
+</script>
