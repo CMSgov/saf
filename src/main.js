7a52b920b38aa406c9318cba30d613aa651a07e8
Will Dower <wdower@mitre.org>
2020-06-26 16:58:14 -0400

    Minor formatting/cleanup tweaks
    Signed-off-by: Will Dower <wdower@mitre.org>

--- src/main.js
+++ src/main.js
@@ -8,18 +8,12 @@ import "./registerServiceWorker";
 import vuetify from "./plugins/vuetify";
 import "./style.css";
 import firebase from "@/firebase";
-//import "material-design-icons-iconfont/dist/material-design-icons.css";
-
-
 import cssVars from "css-vars-ponyfill";
-//import "material-design-icons-iconfont/dist/material-design-icons.css";	//import "material-design-icons-iconfont/dist/material-design-icons.css";
-
 
 cssVars({
   watch: true,
 });
 
-//Vue.use(vueDebounce);	Vue.use(vueDebounce);
 IntersectionObserver.prototype.POLL_INTERVAL = 100; // time in ms
 
 Vue.config.productionTip = false;
