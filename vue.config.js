aa320c0dd70adaa341f103511bdcd2bf40bda24d
Will Dower <wdower@mitre.org>
2020-06-26 14:24:09 -0400

    removed dashify
    Signed-off-by: Will Dower <wdower@mitre.org>

--- vue.config.js
+++ vue.config.js
@@ -9,6 +9,6 @@ module.exports = {
     }
   },
   productionSourceMap: false,
-  transpileDependencies: ["vuetify", "dashify", "register-service-worker"],
+  transpileDependencies: ["vuetify", "register-service-worker"],
   publicPath: process.env.NODE_ENV === "production" ? "/saf/" : "/"
 };
