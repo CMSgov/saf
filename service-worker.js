2a35fc8e86d4717557eb520ba1e090c7ede356fc
aaronlippold <aaronlippold@users.noreply.github.com>
2020-07-19 16:21:20 +0000

    deploy: de9a9f250d4f1bd783f80d2941f45811f8df4e6a

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.d4e7ce8764cbc659fbf6540890b3b0c3.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("/saf/precache-manifest.afed6e3fff4c0a5ff586e14f81f565f7.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 self.addEventListener("message", e => {
   if (!e.data) {
