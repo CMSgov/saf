0acd84f77869df200f18eebe0380ef50f6535007
Will Dower <wdower@mitre.org>
2020-07-31 13:56:34 -0400

    wordsmithing
    Signed-off-by: Will Dower <wdower@mitre.org>

--- src/components/core/FeedbackModal.vue
+++ src/components/core/FeedbackModal.vue
@@ -5,12 +5,25 @@
       <p
         color="white"
       >This website is a work in progress as the CMS SAF is constantly adding new content. We welcome feedback from our users on what content they would like to see on this site.</p>
-      <p>You can reach the CMS SAF team directly by email. Please feel free to open issues on this site's GitHub repository to report bugs or ask for enhancements!</p>
-      <v-card-actions>
-        <v-btn  target="_blank" href="https://github.com/CMSgov/saf/issues/new?labels=bug&template=bug_report.md&title=%5BBUG%5D" color="red">Report a Bug</v-btn>
-        <v-btn  target="_blank" href="https://github.com/CMSgov/saf/issues/new?labels=enhancement&template=feature_request.md&title=" class="primary--text">Request A Feature</v-btn>
-        <v-btn  target="_blank" href="mailto:saf@cms.hhs.gov">Email the CMS SAF Team</v-btn>
-      </v-card-actions>
+      <p>You can reach the CMS SAF team directly by email.</p>
+      <v-btn target="_blank" outlined href="mailto:saf@cms.hhs.gov">Email the CMS SAF Team</v-btn>
+
+      <p class="mt-2">If you have access to github.com/CMSgov, feel free to open an issue to request a feature or report a bug.</p>
+
+        <v-btn
+          target="_blank"
+          href="https://github.com/CMSgov/saf/issues/new?labels=enhancement&template=feature_request.md&title="
+          class="primary--text mr-2"
+          outlined
+        >Request A Feature</v-btn>
+
+        <v-btn
+          target="_blank"
+          href="https://github.com/CMSgov/saf/issues/new?labels=bug&template=bug_report.md&title=%5BBUG%5D"
+          color="red"
+          outlined
+        >Report a Bug</v-btn>
+
     </div>
   </v-card>
 </template>
