a2feb873dec55a783162bda37b7a57de090df198
Will Dower <wdower@mitre.org>
2020-09-08 14:00:52 -0400

    worsmithing, readded the example json
    Signed-off-by: Will Dower <wdower@mitre.org>

--- src/components/normalize/normalizeInfo.vue
+++ src/components/normalize/normalizeInfo.vue
@@ -6,11 +6,17 @@
         <span v-html="row.desc" />
       </p>
       <ul v-if="row.bullets" class="ma-2">
-        <li v-for="bullet in row.bullets" :key="bullet">{{bullet}}</li>
+        <li v-for="(bullet, index) in row.bullets" :key="index">
+          {{bullet.main}}
+          <ul v-if="bullet.sub">
+            <li v-for="(sub_bullet, index) in bullet.sub" :key="index">{{sub_bullet}}</li>
+          </ul>
+        </li>
       </ul>
       <div v-if="row.examples">
         <div v-for="ex in row.examples" :key="ex" class="ma-4">
-        <p class="subheader ma-0">{{ex.title}}</p>
+          <p class="subheader ma-0">{{ex.title}}</p>
+          <p>{{ex.desc}}</p>
           <prism :language="ex.syntax">{{ex.code}}</prism>
         </div>
       </div>
