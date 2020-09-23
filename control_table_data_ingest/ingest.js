d8290f0719242bbc2742605b7c362f3e0936e9b8
Robert Clark <rbclarkdev@gmail.com>
2020-09-23 13:52:17 -0400

    Revert "Finished the github workflow besides making it go on a schedule 1) Pinned the version of inspec 2) Installed the dependencies for the script (it installs a lot of extra stuff in the dev-dependencies, but not sure how to address that) 3) It should now correctly push the changes up to the repo"
    This reverts commit d8c317f77390d10ea775a137ab5313145851f239.

--- control_table_data_ingest/ingest.js
+++ control_table_data_ingest/ingest.js
@@ -2,165 +2,41 @@ const fs = require('fs').promises;
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 
-const inspec = require('inspecjs');
-const parse = require('csv-parse/lib/sync');
-
-const getAllControlsCanonized = () => {
-  const frontier = inspec.nist.FULL_NIST_HIERARCHY;
-  let allControls = [];
-  while (frontier.length) {
-    const { control, children } = frontier.pop();
-    frontier.push(...children);
-    allControls.push(control);
-  }
-
-  allControls = allControls.filter(x => x.sub_specifiers.length > 1);
-  allControls = allControls.sort((a, b) => a.localCompare(b));
-  console.log('all controls', allControls);
-
-  const allControlsCanonized = [];
-  for (const control of allControls) {
-    const canonized = control.canonize({max_specifiers: 3, allow_letters: false, add_spaces: false});
-    console.log('canonized', canonized);
-    if (!allControlsCanonized.includes(canonized)) {
-      allControlsCanonized.push(canonized);
-    }
-  }
-  console.log('all controls canonized', allControlsCanonized);
-
-  return allControlsCanonized;
-}
-
-const downloadGitRepo = async (profile) => {
-  if (profile.link.startsWith('https://github.com/vmware')) {
-    try {
-      const { stdout: gitStdout, stderr: gitStderr } = await exec(`[ ! -d "./vmware" ] && git clone --depth=1 https://github.com/vmware/dod-compliance-and-automation.git "./vmware"`);
-      console.log('stdout:', gitStdout);
-      console.log('stderr:', gitStderr);
-    } catch (error) {
-      console.log('git error that we\'re just gonna ignore cause it\'s probably the "is this directory already here" conditional failing which is the point cause git clone fails on attempted overwrite', error);
-    }
-  } else if (profile.longName.includes('Google')) {
-    console.log('skipping google cause their repos aren\'t set up for inspec json nor do they generate validate profiles');
-  } else if (profile.shortName === 'AWS S3' || profile.shortName === 'AWS RDS CIS') {
-    console.log('skipping these two aws repos due to inspec issues');
-  } else {
-    const { stdout: gitStdout, stderr: gitStderr } = await exec(`git clone --depth=1 ${profile.link} "${profile.shortName}"`);
-    console.log('stdout:', gitStdout);
-    console.log('stderr:', gitStderr);
-  }
-}
-
-const generateProfileJson = async (profile) => {
-  const maxBuffer = 1024 * 1024 * 100; // set max buffer to 100x default size - hopefully ~100MB is sufficient space
-  let inspecStdout, inspecStderr;
-
-  if (profile.link.startsWith('https://github.com/vmware')) {
-    ({ stdout: inspecStdout, stderr: inspecStderr } = await exec(`inspec json --chef-license=accept-silent "./vmware/${profile.link.substr(profile.link.indexOf('vsphere')).concat(profile.link.includes('vcsa') ? '/wrapper' : '')}"`, { maxBuffer: maxBuffer }));
-  } else if (profile.longName.includes('Google') || profile.shortName === 'AWS S3' || profile.shortName === 'AWS RDS CIS') {
-    inspecStdout = await fs.readFile(`./control_table_data_ingest${profile.link.substr(profile.link.lastIndexOf('/'))}.json`, 'utf8');
-  } else {
-    ({ stdout: inspecStdout, stderr: inspecStderr } = await exec(`inspec json --chef-license=accept-silent "./${profile.shortName}"`, { maxBuffer: maxBuffer }));
-  }
-  // console.log('stdout:', inspecStdout); // a very large amount of text
-  console.log('stderr:', inspecStderr);
-
-  return inspecStdout;
-}
-
-const getProfileControls = (allControls, profileText) => {
-  const hdf = inspec.parse.convertFile(profileText);
-  console.log('hdf', hdf);
-
-  const profiles = hdf["1_0_ExecJson"] ? hdf["1_0_ExecJson"].profiles : [hdf["1_0_ProfileJson"]];
-  console.log('profiles', profiles);
-
-  const controls = [];
-  for (const profile of profiles) {
-    for (const control of profile.controls) {
-      controls.push(inspec.hdfWrapControl(control));
-    }
-  }
-  console.log('controls', controls);
-
-  const nistTagHits = new Set();
-  for (const control of controls) {
-    for (const tag of control.parsed_nist_tags) {
-      const canonized = tag.canonize({max_specifiers: 3, allow_letters: false, add_spaces: false});
-      nistTagHits.add(canonized);
-      if (!allControls.includes(canonized)) {
-        throw 'Missed: ' + canonized + ' in ' + tag;
-      }
-    }
-  }
-  console.log('nist tag hits', nistTagHits);
-
-  return [...nistTagHits];
-}
-
-const controlsType = 'NIST SP 800-53 Control';
-
 (async () => {
   try {
-    const allControls = getAllControlsCanonized();
-
-    let { baselines, extras } = JSON.parse(await fs.readFile('./src/assets/data/baselines.json', 'utf8'));
+    const { baselines } = JSON.parse(await fs.readFile('./src/assets/data/baselines.json', 'utf8'));
     console.log(baselines);
 
-    const allProfileControls = {};
-    for (const profile of baselines) {
-      await downloadGitRepo(profile);
+    const profiles = {};
 
-      let profileText = '';
+    for (const profile of baselines) {
+      if (profile.link.startsWith('https://github.com/vmware')) {
+        try {
+          const { stdout: gitStdout, stderr: gitStderr } = await exec(`[ ! -d "./vmware" ] && git clone --depth=1 https://github.com/vmware/dod-compliance-and-automation.git "./vmware"`);
+          console.log('stdout:', gitStdout);
+          console.log('stderr:', gitStderr);
+        } catch (error) {
+          console.log('git error that we\'re just gonna ignore cause it\'s probably the "is this directory already here" conditional failing which is the point cause git clone fails on attempted overwrite', error);
+        }
+      } else {
+        const { stdout: gitStdout, stderr: gitStderr } = await exec(`git clone --depth=1 ${profile.link} "${profile.shortName}"`);
+        console.log('stdout:', gitStdout);
+        console.log('stderr:', gitStderr);
+      }
 
       try {
-        profileText = await generateProfileJson(profile);
-        console.log('profile text', profileText);
+        const { stdout: inspecStdout, stderr: inspecStderr } = await exec(`inspec json --chef-license=accept-silent "./${profile.link.startsWith('https://github.com/vmware') ? "vmware/".concat(profile.link.substr(profile.link.indexOf('vsphere'))).concat(profile.link.includes('vcsa') ? '/wrapper' : '') : profile.shortName}"`, { maxBuffer: 1024 * 1024 * 100 }); // set max buffer to 100x default size - hopefully ~100MB is sufficient space
+        // console.log('stdout:', inspecStdout); // a very large amount of text
+        console.log('stderr:', inspecStderr);
+
+        profiles[profile.shortName] = JSON.parse(inspecStdout);
       } catch (error) {
         console.log('inspec error that we\'re just gonna ignore', error);
       }
-
-      try {
-        allProfileControls[profile.shortName] = getProfileControls(allControls, profileText);
-        console.log('controls that the profile has', profile.shortName, allProfileControls[profile.shortName]);
-      } catch (error) {
-        console.log('inspecjs error that we\'re not gonna ignore', error);
-        throw error;
-      }
     }
-
-    const allExtraControls = {};
-    for (const extra of extras.csv) {
-      await downloadGitRepo(extra);
-
-      const csv = await fs.readFile(`./${extra.path}`, 'utf8');
-      const asJson = parse(csv, { columns: true, skip_empty_lines: true });
-
-      allExtraControls[extra.longName] = asJson.reduce((acc, row) => acc.includes(row[extra.column]) ? acc : acc.concat(row[extra.column]), []);
-      console.log('controls that the extra thing has', extra.longName, allExtraControls[extra.longName]);
-    }
-
-    const controlMapping = [];
-    for (const control of allControls) {
-      const mappingProfile = {};
-      for (const [profileName, profileControls] of Object.entries(allProfileControls)) {
-        mappingProfile[profileName + ' InSpec Profile'] = profileControls.includes(control);
-      }
-
-      const mappingExtra = {};
-      for (const [extraName, extraControls] of Object.entries(allExtraControls)) {
-        mappingExtra[extraName] = extraControls.includes(control);
-      }
-
-      const all = Object.keys(mappingProfile).reduce((acc, key) => acc || mappingProfile[key], false) || Object.keys(mappingExtra).reduce((acc, key) => acc || mappingExtra[key], false);
-
-      controlMapping.push({ [controlsType]: control, "ALL": all, ...mappingExtra, ...mappingProfile });
-    }
-    console.log('control mapping', controlMapping);
-
-    await fs.writeFile('./src/assets/data/mitre-saf-control-mapping.json', JSON.stringify(controlMapping), 'utf8');
-    console.log('hopefully we\'re in a success state now');
   } catch (error) {
     console.log('error', error);
   }
+
+  await fs.writeFile('./testing', 'yeeting my way to victory', 'utf8');
 })();
