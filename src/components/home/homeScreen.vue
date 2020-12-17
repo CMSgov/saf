046d81a5b6921548794be058ceec04d9188c4f68
Will Dower <wdower@mitre.org>
2020-12-17 18:55:54 -0500

    resolves #77
    Signed-off-by: Will Dower <wdower@mitre.org>

--- src/components/home/homeScreen.vue
+++ src/components/home/homeScreen.vue
@@ -25,7 +25,7 @@
         <p class=" ms-0 title">{{communityData.communityShortDescription}}</p>
         <p class="ma-0">
           <ul class="title mt-4">
-            <li>The ACT (Adaptive Capabilities Testing) team will accept security testing data from this framework.</li>
+            <li>The ACT (Adaptive Capabilities Testing) team will accept security testing data from this framework. See the <router-link to="/faq#act">FAQ</router-link> for more on the ACT program.</li>
             <li>Developers can harden and run validation security early and often in their environments, using their own orchestration, functional and unit testing systems, to keep security defects as low as possible.</li>
           </ul>
         </p>
