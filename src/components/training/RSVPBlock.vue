d088940b01c4319e04142d78462b919222773790
Will Dower <wdower@mitre.org>
2021-02-26 13:49:00 -0500

    broke out the RSVP code into a component controlled by JSON to keep it easy to manage
    Signed-off-by: Will Dower <wdower@mitre.org>

---
+++ src/components/training/RSVPBlock.vue
@@ -0,0 +1,84 @@
+<template>
+  <div>
+    <h2>RSVP – InSpec profile developer training for March/April 2021!</h2>
+    <p>
+      Sponsored by CMS under the CMS Security Automation Framework, we’re happy
+      to announce plans to again provide training to developers interested in
+      learning to create and maintain InSpec profiles.
+    </p>
+    <p>
+      InSpec profiles are a core tool in establishing tailored, transparent,
+      open-source community-based automated security testing content.
+    </p>
+    <p>
+      These are free, on-line virtual, hands-on, instructor-led 2-day classes.
+    </p>
+    <p>
+      The
+      <a :href="communityData.trainingSiteBasic" target="_blank"
+        >base introductory InSpec developer class</a
+      >
+      will again be offered on March 30-31, 2021 (9am-4pm both days)
+    </p>
+    <p>
+      The
+      <a
+        :href="communityData.trainingSiteAdvanced"
+        target="_blank"
+        >follow-on Advanced InSpec developer class</a
+      >
+      will again be offered on April 13-14, 2021 (9am-4pm both days)
+    </p>
+    <p>
+      Please RSVP if you are interested in attending or have additional
+      questions. If you are having trouble with the form, please email
+      <a :href="communityData.trainingContactEmail" target="_blank"
+        >SAF@groups.mitre.org.</a
+      >
+    </p>
+    <v-btn
+      elevation="8"
+      large
+      outlined
+      text
+      x-large
+      :href="communityData.trainingSignUpLink"
+      target="_blank"
+      class="ma-8"
+      color="accent"
+      >RSVP!</v-btn
+    >
+    <p>
+      A note on prerequisites: For the basic class, knowledge of basic
+      programming concepts, modern language conventions and some working
+      experience with a modern language (e.g., Ruby, Python, Go, etc) is useful.
+      (Inspec is a Ruby Domain-specific language (DSL)). To become more familiar
+      with Ruby, feel free to review the following Ruby primers. For the
+      follow-on advanced class (basic is a preferred prerequisite), we'll be
+      writing InSpec Resources in the Ruby language, so it is highly recommended
+      students of the advanced class unfamiliar with Ruby review the following
+      Ruby primers
+      <a
+        href="https://www.ruby-lang.org/en/documentation/quickstart/"
+        target="_blank"
+        >here</a
+      >
+      and on
+      <a
+        href="https://www.youtube.com/watch?v=t_ispmWmdjY&vl=en"
+        target="_blank"
+        >youtube.</a
+      >
+    </p>
+  </div>
+</template>
+<script>
+import communityData from "@/assets/data/communityData.json";
+export default {
+  data() {
+    return {
+      communityData: communityData,
+    };
+  },
+}
+</script>
--- src/views/Training.vue
+++ src/views/Training.vue
@@ -10,83 +10,7 @@
     <v-row justify="center" align="center" class="mx-0">
       <v-col cols="12" lg="10" class="pa-0">
         <v-container fluid>
-          <h2>
-            RSVP – InSpec profile developer training for March/April 2021!
-          </h2>
-          <p>
-            Sponsored by CMS under the CMS Security Automation Framework, we’re
-            happy to announce plans to again provide training to developers
-            interested in learning to create and maintain InSpec profiles.
-          </p>
-          <p>
-            InSpec profiles are a core tool in establishing tailored,
-            transparent, open-source community-based automated security testing
-            content.
-          </p>
-          <p>
-            These are free, on-line virtual, hands-on, instructor-led 2-day
-            classes.
-          </p>
-          <p>
-            The
-            <a
-              href="https://mitre-inspec-developer.netlify.com/"
-              target="_blank"
-              >base introductory InSpec developer class</a
-            >
-            will again be offered on March 30-31, 2021 (9am-4pm both days)
-          </p>
-          <p>
-            The
-            <a
-              href="https://mitre-inspec-advanced-developer.netlify.app/"
-              target="_blank"
-              >follow-on Advanced InSpec developer class</a
-            >
-            will again be offered on April 13-14, 2021 (9am-4pm both days)
-          </p>
-          <p>
-            Please RSVP if you are interested in attending or have additional
-            questions. If you are having trouble with the form, please email
-            <a href="mailto:SAF@groups.mitre.org" target="_blank"
-              >SAF@groups.mitre.org.</a
-            >
-          </p>
-          <v-btn
-            elevation="8"
-            large
-            outlined
-            text
-            x-large
-            href="https://forms.office.com/Pages/ResponsePage.aspx?id=SNwgxlAdUkmLOd9NVNdNgkOA91ej9MNHss6_kcyA_29UMFAzTkFSSkFaMVdWRUZFREo5QzBNTkUyRy4u"
-            target="_blank"
-            class="ma-8"
-            color="accent"
-            >RSVP!</v-btn
-          >
-          <p>
-            A note on prerequisites: For the basic class, knowledge of basic
-            programming concepts, modern language conventions and some working
-            experience with a modern language (e.g., Ruby, Python, Go, etc) is
-            useful. (Inspec is a Ruby Domain-specific language (DSL)). To become
-            more familiar with Ruby, feel free to review the following Ruby
-            primers. For the follow-on advanced class (basic is a preferred
-            prerequisite), we'll be writing InSpec Resources in the Ruby
-            language, so it is highly recommended students of the advanced class
-            unfamiliar with Ruby review the following Ruby primers
-            <a
-              href="https://www.ruby-lang.org/en/documentation/quickstart/"
-              target="_blank"
-              >here</a
-            >
-            and on
-            <a
-              href="https://www.youtube.com/watch?v=t_ispmWmdjY&vl=en"
-              target="_blank"
-              >youtube.</a
-            >
-          </p>
-
+          <RSVPBlock />
           <h2>About CMS SAF Training</h2>
           <p>
             InSpec is an open-source and precise scripting framework to help you
@@ -136,6 +60,7 @@
 // import pastTraining from "../components/training/traininginfo";
 import communityData from "@/assets/data/communityData.json";
 import Header from "@/components/core/Header.vue";
+import RSVPBlock from "@/components/training/RSVPBlock.vue"
 
 export default {
   data() {
@@ -145,6 +70,7 @@ export default {
   },
   components: {
     Header,
+    RSVPBlock
     // pastTraining
   },
   created() {
