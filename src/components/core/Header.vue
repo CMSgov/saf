46d59ba207bc83e8697de497929770f6dd0179a0
Will Dower <wdower@mitre.org>
2021-04-28 10:51:11 -0400

    added updated DSO best practices guide
    Signed-off-by: Will Dower <wdower@mitre.org>

--- src/components/core/Header.vue
+++ src/components/core/Header.vue
@@ -1,5 +1,5 @@
 <template>
-  <div :class="$vuetify.breakpoint.mdAndDown ? 'pa-2' : ''">
+  <div :class="$vuetify.breakpoint.mdAndDown ? 'pa-2' : 'py-2'">
     <div class="google-font primary--text pa-0 ma-0" style="font-weight: 350; font-size: 2.0rem">
       <slot name="title"></slot>
     </div>
