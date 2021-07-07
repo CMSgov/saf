36d783bee4a86be1f76e30398dd55c4c1d55612f
Will Dower <wdower@mitre.org>
2021-07-06 22:00:36 -0400

    working out tests
    Signed-off-by: Will Dower <wdower@mitre.org>

---
+++ cypress/integration/homepage.js
@@ -0,0 +1,17 @@
+describe('The Home Page', () => {
+    it('loads all sections', () => {
+      // go to homepage
+      cy.visit('/')
+      // check the top text
+      cy.contains('The CMS Security Automation Framework (SAF) brings together applications, techniques, libraries')
+      // check How CMS SAF Works
+      cy.contains('(Code Analysis, Continuous Monitoring, Security Operations)')
+      // check a chevron
+      cy.get('[id=operations_chevron]').should('be.visible')
+      cy.contains('')
+      cy.contains('')
+      cy.contains('')
+      cy.contains('')
+
+    })
+  })
