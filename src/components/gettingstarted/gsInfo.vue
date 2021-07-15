1d465abae6a80db42da8bad832179d181e2e0e3d
Will Dower <wdower@mitre.org>
2021-07-15 15:22:10 -0400

    fixed bug in baseURL (need to account for '#' in vue urls), restructured site-tests.yml to split up the build, deploy and test steps, added new tests for getting started page
    Signed-off-by: Will Dower <wdower@mitre.org>

--- src/components/gettingstarted/gsInfo.vue
+++ src/components/gettingstarted/gsInfo.vue
@@ -26,13 +26,18 @@
           :key="entry.name"
         >
           <span>
-            <a v-if="entry.link" :href="entry.link" target="_blank">{{
-              entry.name
-            }}</a>
+            <a
+              v-if="entry.link"
+              :href="entry.link"
+              target="_blank"
+              :id="'gs-link ' + entry.name"
+              >{{ entry.name }}</a
+            >
             <a
               v-if="entry.download_link"
               :href="entry.download_link"
               target="_blank"
+              :id="'gs-dl-link ' + entry.name"
               download
               >{{ entry.name }}</a
             >
@@ -41,7 +46,8 @@
         </p>
         <v-img
           v-if="item.image"
-          :src="require('@/assets/img/tools/' + item.image)"
+          :id="item.image"
+          :src="require('@/assets/img/tools/' + item.image + '.png')"
         />
       </div>
     </template>
@@ -49,71 +55,71 @@
 </template>
 
 <script>
-import communityData from "@/assets/data/communityData.json";
-import trainingData from "@/assets/data/trainingData.json";
-import resources from "@/assets/data/resources.json";
-import VideoCard from "@/components/gettingstarted/VideoCard.vue";
-export default {
-  name: "App",
-  data: () => ({
-    communityData: communityData,
-    trainingData: trainingData,
-    resources: resources,
-    dialog: false,
-    dialogData: {
-      name: "",
-      date: "",
-      link: "",
+  import communityData from "@/assets/data/communityData.json";
+  import trainingData from "@/assets/data/trainingData.json";
+  import resources from "@/assets/data/resources.json";
+  import VideoCard from "@/components/gettingstarted/VideoCard.vue";
+  export default {
+    name: "App",
+    data: () => ({
+      communityData: communityData,
+      trainingData: trainingData,
+      resources: resources,
+      dialog: false,
+      dialogData: {
+        name: "",
+        date: "",
+        link: "",
+      },
+      heimdall_video: {
+        name: "VIDEO: Heimdall Docker Installation",
+        image: "heimdall",
+        link: "https://youtu.be/irFLngy1PCA",
+        height: "200px",
+      },
+      cinc_video: {
+        name: "VIDEO: CINC-Auditor Installation",
+        image: "cinc",
+        link: "https://youtu.be/HSFWE3KEu7o",
+        height: "200px",
+      },
+    }),
+    components: {
+      VideoCard: VideoCard,
     },
-    heimdall_video: {
-      name: "VIDEO: Heimdall Docker Installation",
-      image: "heimdall",
-      link: "https://youtu.be/irFLngy1PCA",
-      height: "200px",
+    computed: {
+      color_mode() {
+        if (this.$vuetify.theme.dark) return "darkModeContainer";
+        return "lightModeContainer";
+      },
+      moveForBottomNavStyle() {
+        if (this.$vuetify.breakpoint.smAndDown) {
+          return "z-index: 10; bottom: 60px";
+        } else {
+          return "";
+        }
+      },
     },
-    cinc_video: {
-      name: "VIDEO: CINC-Auditor Installation",
-      image: "cinc",
-      link: "https://youtu.be/HSFWE3KEu7o",
-      height: "200px",
+    methods: {
+      make_linkable(str) {
+        return str.replace(/\s+/g, "-").toLowerCase();
+      },
+      passItemData(item) {
+        this.dialogData = item;
+      },
     },
-  }),
-  components: {
-    VideoCard: VideoCard
-  },
-  computed: {
-    color_mode() {
-      if (this.$vuetify.theme.dark) return "darkModeContainer";
-      return "lightModeContainer";
-    },
-    moveForBottomNavStyle() {
-      if (this.$vuetify.breakpoint.smAndDown) {
-        return "z-index: 10; bottom: 60px";
-      } else {
-        return "";
-      }
-    },
-  },
-  methods: {
-    make_linkable(str) {
-      return str.replace(/\s+/g, "-").toLowerCase();
-    },
-    passItemData(item) {
-      this.dialogData = item;
-    },
-  },
-};
+  };
 </script>
 
 <style scoped>
-.wrap-list-text {
-  -webkit-line-clamp: unset !important;
-  white-space: normal;
-  overflow-wrap: break-word;
-  word-wrap: break-word;
-  word-break: normal;
-  hyphens: none;
-  font-size: 150%;
-  color: #1a73e8;
-}
+  .wrap-list-text {
+    -webkit-line-clamp: unset !important;
+    white-space: normal;
+    overflow-wrap: break-word;
+    word-wrap: break-word;
+    word-break: normal;
+    hyphens: none;
+    font-size: 150%;
+    color: #1a73e8;
+  }
 </style>
