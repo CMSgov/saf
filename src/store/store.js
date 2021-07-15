d83a35816ba3ded6450c0ab28894b3f94bb4e74c
Will Dower <wdower@mitre.org>
2021-07-15 13:07:13 -0400

    Revert "updated What's New"
    This reverts commit e708a7fd27ee669c7f9881f2b2f8bacc2966070d.

--- src/store/store.js
+++ src/store/store.js
@@ -113,7 +113,7 @@ export default new Vuex.Store({
         }
       }, */
       {
-        text: "What's New [Now Run InSpec using AWS Lambda]",
+        text: "What's New [Now Automate Manual Controls!]",
         to: "/whats-new",
         icon: "mdi-newspaper",
         meta: {
