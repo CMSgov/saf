d1df7d5998ad2fe06e9cd6ca38ef5b188f4c9144
Will Dower <wdower@mitre.org>
2021-07-23 11:04:17 -0400

    took out the intentionally failing test to see if the deployment action will fire after successfult test
    Signed-off-by: Will Dower <wdower@mitre.org>

--- cypress/integration/validate.js
+++ cypress/integration/validate.js
@@ -11,5 +11,5 @@ describe('The Validate Page', () => {
       cy.contains('MongoDB STIG')
       cy.contains('RSA Archer 6 SCG')
       cy.contains('NGINX')
-      cy.contains('This test should fail for workflow testing purporses.')
   })
+})
