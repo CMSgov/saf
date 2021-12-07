e71fe069a810b9716f848140cb34de028263c781
Will Dower <wdower@mitre.org>
2021-12-07 10:15:22 -0500

    added rhel-8-baseline, fixed a margin issue along the bottom of some views
    Signed-off-by: Will Dower <wdower@mitre.org>

--- src/views/Faq.vue
+++ src/views/Faq.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-container fluid class="pa-0 ma-0">
+  <v-container fluid class="pa-0 pb-4 ma-0">
     <v-row justify="center" align="center" class="mx-0">
       <v-col cols="12" lg="10" class="pa-0">
         <Header>
@@ -7,10 +7,10 @@
           <p slot="subtitle">
             Have a question you don't see covered here? Please contact
             <a
-              style="text-decoration: none;"
+              style="text-decoration: none"
               :href="`mailto:${communityData.communityEmail}`"
             >
-              {{communityData.communityEmail}}
+              {{ communityData.communityEmail }}
             </a>
             .
           </p>
@@ -26,19 +26,19 @@
 </template>
 
 <script>
-import Header from "@/components/core/Header.vue";
-import faqInfo from "@/components/FAQ/faqInfo.vue";
-import communityData from "@/assets/data/communityData.json";
+  import Header from "@/components/core/Header.vue";
+  import faqInfo from "@/components/FAQ/faqInfo.vue";
+  import communityData from "@/assets/data/communityData.json";
 
-export default {
-  data() {
-    return {
-      communityData: communityData
-    };
-  },
-  components: {
-    Header,
-    faqInfo
-  }
-};
+  export default {
+    data() {
+      return {
+        communityData: communityData,
+      };
+    },
+    components: {
+      Header,
+      faqInfo,
+    },
+  };
 </script>
