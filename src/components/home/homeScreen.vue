8cabbaf61081f16c51ea8a9ea92afb2a359432d4
Eugene Aronne <34140975+ejaronne@users.noreply.github.com>
2023-04-26 13:17:48 -0400

    Update homeScreen.vue

--- src/components/home/homeScreen.vue
+++ src/components/home/homeScreen.vue
@@ -11,6 +11,8 @@
           <ul class="title mt-4">
               <li>The ACT (Adaptive Capabilities Testing) team will accept security testing data from this framework. See the <router-link to="/faq#act">FAQ</router-link> for more on the ACT program.</li>
               <li>Developers can harden and run validation security early and often in their environments, using their own orchestration, functional and unit testing systems, to keep security defects as low as possible.</li>
+              <li>ARS (Acceptable Risk Safeguards) 5 Support: CMS SAF has updated relevant InSpec profiles (see “ARS 5” links on our Validate page) to allow teams to validate against CMS’ latest organizational-specific requirements.</li>
+              <li>Open-source tool support: The SAF CLI’s \"convert\" tool continues to support import/export/interchange of security data for a variety of open-source and vendor security tools such as AWS Security Hub, Snyk, Scoutsuite, and Splunk.</li>
           </ul>
       </p>
   </v-container>
